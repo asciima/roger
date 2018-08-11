55227d1866146389baa88b039f108542e4c2af6d
ycopy <io.cpp@outlook.com>
2018-08-12 00:41:54 +0800

    [opt]opt https connect, dns query

--- trunk/projects/roger/src/roger/server/server_handlers.hpp
+++ trunk/projects/roger/src/roger/server/server_handlers.hpp
@@ -280,8 +280,8 @@ namespace roger {
 				WAWO_ERR("[server][forward_ctx][%s][s%d]dns(%s) lookup failed: %d, try time: %u", server_state_str[fctx->state], fctx->ch_stream_ctx->ch->ch_id(), fctx->dst_domain.c_str(), code, fctx->dns_try_time);
 				fctx->query = dns_resolver::instance()->async_resolve(fctx->dst_domain, fctx, &roger::dns_resolve_success, &roger::dns_resolve_error);
 				++fctx->dns_try_time;
-				return;
 			});
+			return;
 		}
 
 		fctx->ch_stream_ctx->event_poller()->execute([fctx,code]() {
