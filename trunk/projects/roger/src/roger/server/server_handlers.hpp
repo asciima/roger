428ac9a128806585fc87c2bb9d3ee081bf18039e
ycopy <io.cpp@outlook.com>
2018-11-04 22:19:27 +0800

    opt flush_up & flush_down

--- trunk/projects/roger/src/roger/server/server_handlers.hpp
+++ trunk/projects/roger/src/roger/server/server_handlers.hpp
@@ -15,8 +15,13 @@ namespace roger {
 		switch (fctx->state) {
 			case LOOKUP_SERVER_NAME:
 			case DIAL_SERVER:
+			case DIAL_SERVER_OK:
+			{
+			}
+			break;
 			case SERVER_CONNECTED:
 			{
+				WAWO_ASSERT(fctx->ch_server_ctx != NULL);
 				if (fctx->up_to_server_packets.size()) {
 					WWRP<wawo::net::channel_promise> f = fctx->ch_server_ctx->make_channel_promise();
 					f->add_listener([fctx](WWRP<wawo::net::channel_future> const& f) {
@@ -33,10 +38,8 @@ namespace roger {
 				}
 				else {
 					if (fctx->stream_read_closed) {
-						if (fctx->ch_server_ctx != NULL) {
-							TRACE_SERVER_SIDE_CTX("[server][s%u]no up to server packets left and stream read closed, close server write", fctx->ch_stream_ctx->ch->ch_id());
-							fctx->ch_server_ctx->shutdown_write();
-						}
+						TRACE_SERVER_SIDE_CTX("[server][s%u]no up to server packets left and stream read closed, close server write", fctx->ch_stream_ctx->ch->ch_id());
+						fctx->ch_server_ctx->shutdown_write();
 					}
 				}
 			}
@@ -67,12 +70,12 @@ namespace roger {
 			fctx->ch_server_ctx->close();
 		}
 	}
-	inline void flush_up(WWRP<forward_ctx> const& fctx, WWRP<wawo::packet> const& income, bool flush = true ) {
+	inline void flush_up(WWRP<forward_ctx> const& fctx, WWRP<wawo::packet> const& income ) {
 		WAWO_ASSERT(fctx->ch_stream_ctx->event_poller()->in_event_loop());
 		if (income != NULL) {
 			fctx->up_to_server_packets.push(income);
 		}
-		if (fctx->up_state == ctx_write_state::WS_WRITING && flush ) {
+		if (fctx->up_state == ctx_write_state::WS_WRITING ) {
 			return;
 		}
 		_do_flush_up(fctx);
@@ -218,7 +221,7 @@ namespace roger {
 		fctx->state = DIAL_SERVER;
 		fctx->ts_server_connect_start = wawo::time::curr_microseconds();
 
-		std::string dialurl = "tcp://" + fctx->dst_addrv4.dotip() + ":"+ std::to_string(fctx->dst_addrv4.port());
+		const std::string dialurl = "tcp://" + fctx->dst_addrv4.dotip() + ":"+ std::to_string(fctx->dst_addrv4.port());
 		WWRP<wawo::net::channel_future> dial_f = wawo::net::socket::dial(dialurl, [fctx](WWRP<wawo::net::channel> const& ch) {
 			WWRP<server_handler> h = wawo::make_ref<server_handler>();
 			ch->pipeline()->add_last(h);
@@ -238,10 +241,9 @@ namespace roger {
 					WAWO_ASSERT(fctx != NULL);
 					WAWO_ASSERT(fctx->ch_server_ctx == NULL);
 					WAWO_ASSERT(fctx->ch_stream_ctx != NULL);
-					fctx->state = DIAL_SERVER_FAILED;
-					WAWO_ASSERT(fctx->ch_server_ctx == NULL);
 					WAWO_ASSERT(fctx->down_to_stream_packets.size() == 0);
 					fctx->server_read_closed = true;
+					fctx->state = DIAL_SERVER_FAILED;
 					WAWO_ERR("[server][forward_ctx][s%u--%s:%u][%s]dial failed: %d", fctx->ch_stream_ctx->ch->ch_id(), wawo::net::ipv4todotip(fctx->dst_ipv4).c_str(), fctx->dst_port, fctx->dst_domain.c_str(), rt );
 				} else {
 					//TRACE_SERVER_SIDE_CTX("[server][forward_ctx][s%u--%s:%u][%s]dial ok", fctx->ch_stream_ctx->ch->ch_id(), wawo::net::ipv4todotip(fctx->dst_ipv4).c_str(), fctx->dst_port,fctx->dst_domain.c_str() );
@@ -254,9 +256,9 @@ namespace roger {
 				flush_down(fctx, outp);
 
 				WAWO_ASSERT(fctx->ts_dns_lookup_done>=fctx->ts_dns_lookup_start);
-				u64_t dns_lookup_time = fctx->ts_dns_lookup_done - fctx->ts_dns_lookup_start;
+				const u64_t dns_lookup_time = fctx->ts_dns_lookup_done - fctx->ts_dns_lookup_start;
 				WAWO_ASSERT(fctx->ts_server_connect_done>=fctx->ts_server_connect_start);
-				u64_t server_connect_time = fctx->ts_server_connect_done - fctx->ts_server_connect_start;
+				const u64_t server_connect_time = fctx->ts_server_connect_done - fctx->ts_server_connect_start;
 				if (dns_lookup_time > 1000000) {
 					WAWO_INFO("connect statistic: dns_lookup: %.3f ms, connect: %.3f ms, connect result: %d, domain: %s", dns_lookup_time / 1000.0, server_connect_time / 1000.0, rt, fctx->dst_domain.c_str());
 				}
@@ -271,9 +273,8 @@ namespace roger {
 		fctx->ts_dns_lookup_done = wawo::time::curr_microseconds();
 		WAWO_ASSERT(code <= E_DNSLOOKUP_RETURN_NO_IP && code>= E_DNS_BADQUERY );
 
-		if (code == E_DNS_TEMPORARY_ERROR && (fctx->dns_try_time) < 3) {
-
-			WWRP<wawo::timer> retry_timer = wawo::make_ref<wawo::timer>( std::chrono::milliseconds(1*(fctx->dns_try_time+1)<<1),
+		if (code == E_DNS_TEMPORARY_ERROR && (fctx->dns_try_time) < 5) {
+			WWRP<wawo::timer> retry_timer = wawo::make_ref<wawo::timer>( std::chrono::milliseconds((fctx->dns_try_time)<<1),
 				[fctx, code] (WWRP<wawo::timer> const& t) {
 					WAWO_ASSERT(fctx->query != NULL);
 					WAWO_ERR("[server][forward_ctx][%s][s%d]dns(%s) lookup failed: %d, try time: %u", server_state_str[fctx->state], fctx->ch_stream_ctx->ch->ch_id(), fctx->dst_domain.c_str(), code, fctx->dns_try_time);
@@ -281,20 +282,20 @@ namespace roger {
 					++fctx->dns_try_time;
 					(void)t;
 				});
-
 			fctx->ch_stream_ctx->event_poller()->start_timer(retry_timer);
 			return;
 		}
 
 		fctx->ch_stream_ctx->event_poller()->execute([fctx,code]() {
-			WAWO_ASSERT(fctx->query != NULL);
-			fctx->query = NULL;
-			WAWO_ASSERT(fctx->ch_stream_ctx != NULL);
-			fctx->state = LOOKUP_SERVER_NAEM_FAILED;
 			WAWO_ASSERT(code != wawo::OK);
+			WAWO_ASSERT(fctx->query != NULL);
+			WAWO_ASSERT(fctx->ch_stream_ctx != NULL);
 			WAWO_ASSERT(fctx->ch_server_ctx == NULL);
 			WAWO_ASSERT(fctx->down_to_stream_packets.size() == 0);
+
+			fctx->query = NULL;
 			fctx->server_read_closed = true;
+			fctx->state = LOOKUP_SERVER_NAEM_FAILED;
 			WWRP<wawo::packet> outp = wawo::make_ref<wawo::packet>(64);
 			outp->write<int32_t>(code);
 			flush_down(fctx, outp);
@@ -306,7 +307,7 @@ namespace roger {
 		WWRP<forward_ctx> fctx = wawo::static_pointer_cast<forward_ctx>(cookie_);
 		WAWO_ASSERT(fctx->ch_server_ctx == NULL);
 
-		//sometimes we get 0.0.0.0, I don't know why
+		//sometimes we get 0.0.0.0
 		wawo::net::ipv4_t ipv4 = 0;
 		const ::size_t in_addrs_size = in_addrs.size();
 		WAWO_ASSERT(in_addrs_size >0);
@@ -364,7 +365,7 @@ namespace roger {
 
 			TRACE_SERVER_SIDE_CTX("[server][s%u]stream read closed, plan a flush", fctx->ch_stream_ctx->ch->ch_id());
 			fctx->stream_read_closed = true;
-			flush_up(fctx, NULL);
+			flush_up(fctx,NULL);
 		}
 
 		void closed(WWRP<wawo::net::channel_handler_context> const& ctx) {
@@ -386,18 +387,18 @@ namespace roger {
 
 			bool write_income_done = false;
 
-		_begin:
+		_read_begin:
 			switch (fctx->state)
 			{
 			case CONNECT:
 			{
 				fctx->client_up_first_packet->write(income->begin(), income->len());
 				write_income_done = true;
-				if (fctx->client_up_first_packet->len()<6) {
+				if (fctx->client_up_first_packet->len()<4) {
 					return;
 				}
 
-				u8_t cmd = fctx->client_up_first_packet->read<u8_t>();
+				const u8_t cmd = fctx->client_up_first_packet->read<u8_t>();
 				if (cmd != C_CONNECT) {
 					WAWO_ERR("[server][s%u] command: %u", ctx->ch->ch_id(), cmd);
 					fctx->server_read_closed = true;
@@ -410,7 +411,7 @@ namespace roger {
 				fctx->dst_port = fctx->client_up_first_packet->read<u16_t>();
 				fctx->address_type = (roger_connect_address_type)fctx->client_up_first_packet->read<u8_t>();
 				fctx->state = READ_DST_ADDR;
-				if (fctx->client_up_first_packet->len()) { goto _begin; }
+				if (fctx->client_up_first_packet->len()) { goto _read_begin; }
 			}
 			break;
 			case READ_DST_ADDR:
@@ -498,12 +499,14 @@ namespace roger {
 			break;
 			case SERVER_CONNECTED:
 			{
+				WAWO_ASSERT(fctx->client_up_first_packet->len() == 0);
 				flush_up(fctx, income);
 			}
 			break;
 			case DIAL_SERVER_FAILED:
 			case LOOKUP_SERVER_NAEM_FAILED:
 			{
+				WAWO_ASSERT(fctx->server_read_closed == true);
 				TRACE_SERVER_SIDE_CTX("[server][s%u][%s]error state, close stream", fctx->ch_stream_ctx->ch->ch_id(), server_state_str[fctx->state]);
 				WAWO_ASSERT(fctx->ch_stream_ctx != NULL);
 				fctx->ch_stream_ctx->close();
